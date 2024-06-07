acfbe68316484a14c924688ff7cfac5527338ebe
OlyaBaeva <olyabaewa2014@mail.ru>
2024-06-08 00:14:12 +0300

    add filter and search by book title

--- src/App.js
+++ src/App.js
@@ -1,45 +1,12 @@
-/*import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-*/
-
-
 import React from 'react';
 import './App.css';
 import Epoch from'./Epoch';
-
-
-const books = [
-  { ID: 1, Name: 'Отель у погибшего альпиниста', year: '1969', path:"1.jpg" },
-  { ID: 2, Name: 'Понедельник начинается в субботу', year: '1965', path:"2.jpg" },
-  { ID: 3, Name: 'Страна Багровых туч', year: '1959', path:"3.jpg" },              
-];
 
 function App() {
   return (
       <div>
       
-      <Epoch books={books}/>
+      <Epoch/>
       </div>
       );
 }
--- src/Epoch.js
+++ src/Epoch.js
@@ -1,20 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import Table from 'react-bootstrap/Table';
-import { Button } from 'react-bootstrap';
+import React, { useState, useEffect, useRef } from "react";
+import Table from "react-bootstrap/Table";
+import { Button } from "react-bootstrap";
 import "./App.css";
 import * as XLSX from "xlsx";
 
 const Epoch = () => {
   const [books, setBooks] = useState([]);
-  const [sortOrder, setSortOrder] = useState('asc');
-  const [quote, setQuote] = useState('');
-  const [title, setTitle] = useState('');
-  const [data, setData] = useState([]);
+  const [sortOrder, setSortOrder] = useState("asc");
+  const [quote, setQuote] = useState("");
+  const [title, setTitle] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+  const initialLoad = useRef(true);
+
   useEffect(() => {
-    fetch(process.env.PUBLIC_URL + '/Stru1.xls')
-      .then(response => response.arrayBuffer())
-      .then(data => {
-        const workbook = XLSX.read(data, { type: 'array' });
+    fetch(process.env.PUBLIC_URL + "/Stru1.xls")
+      .then((response) => response.arrayBuffer())
+      .then((data) => {
+        const workbook = XLSX.read(data, { type: "array" });
         const sheetName = workbook.SheetNames[0];
         const sheet = workbook.Sheets[sheetName];
         const jsonData = XLSX.utils.sheet_to_json(sheet);
@@ -24,66 +26,128 @@ const Epoch = () => {
 
         setQuote(randomQuote);
         setTitle(randomTitle);
-        console.log(jsonData[randomIndex].quote);
       })
-      .catch(error => console.log(error));
+      .catch((error) => console.log(error));
   }, []);
 
   useEffect(() => {
-    fetch('https://www.googleapis.com/books/v1/volumes?q=strugatsky+brothers')
-      .then(response => response.json())
-      .then(data => {
-        setBooks(data.items);
-      })
-      .catch(error => {
-        console.error('Error fetching books:', error);
-      });
-  }, []);
+    const getBooks = async () => {
+      try {
+        const response = await fetch(
+          "https://www.googleapis.com/books/v1/volumes?q=inauthor:strugatsky+brothers&maxResults=20"
+        );
+        const data = await response.json();
+
+        if (data && data.items) {
+          const bookList = data.items.map(({ volumeInfo }) => ({
+            title: volumeInfo.title,
+            authors: volumeInfo.authors,
+            description: volumeInfo.description? volumeInfo.description: 'Unknown',
+            printType: volumeInfo.printType,
+          }));
+          if (initialLoad.current) {
+            setBooks(bookList);
+            initialLoad.current = false;
+          }
+        }
+      } catch (error) {
+        console.error("Error fetching books:", error);
+      }
+    };
+
+    getBooks();
+  }, [books]);
+
+  const uniqueAuthors = [
+    ...new Set(books.map((book) => book.authors.sort().join(", "))),
+  ];
+
+  const [selectedAuthor, setSelectedAuthor] = useState("");
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSelectPublisher = (event) => {
+    setSelectedAuthor(event.target.value);
+  };
+  const handleClearFields = () => {
+    setSearchTerm('');
+  };
 
   const handleSort = () => {
-    const sortedBooks = [...books].sort((a, b) => {
-      if (sortOrder === 'asc') {
-        return a.volumeInfo.title.localeCompare(b.volumeInfo.title);
+    const booksCopy = [...books];
+    const sortedBooks = [...booksCopy].sort((a, b) => {
+      if (sortOrder === "asc") {
+        return a.title.localeCompare(b.title);
       } else {
-        return b.volumeInfo.title.localeCompare(a.volumeInfo.title);
+        return b.title.localeCompare(a.title);
       }
     });
 
     setBooks(sortedBooks);
-    setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+    setSortOrder(sortOrder === "asc" ? "desc" : "asc");
   };
 
   return (
     <div>
       <div className="container">
-      <figure className="text-end">
-        <blockquote className="blockquote">
-          <p> {quote} </p>
-        </blockquote>
-        <figcaption className="blockquote-footer">
-          <cite title="Source Title">{title}</cite>
-        </figcaption>
-      </figure>
+        <figure className="text-end">
+          <blockquote className="blockquote">
+            <p> {quote} </p>
+          </blockquote>
+          <figcaption className="blockquote-footer">
+            <cite title="Source Title">{title}</cite>
+          </figcaption>
+        </figure>
       </div>
-
-          <Button className="btn btn-danger" onClick={handleSort}>Sort by Title</Button>
-
+      <select style={{ justifyContent: "center", textAlign: "center" }} className="form-select-sm" onClick={handleSelectPublisher}>
+        <option value="" >Author</option>
+        {uniqueAuthors.map((author) => (
+          <option key={books.id}>{author}</option>
+        ))}
+      </select>
+      <Button variant="outline-secondary float-end" size="sm" onClick={handleClearFields}>Clear Fields</Button>
+      <input className="form-control-sm float-end" type="text" value={searchTerm} onChange={handleChange} placeholder="Enter book title" style={{ marginLeft: "40px" }}/>
+      
       <Table className="table" striped bordered hover variant="dark">
         <thead style={{ textAlign: "center" }}>
-          <tr>
-            <th>Book</th>
+          <tr style={{textAlign: "center", verticalAlign: "middle"}}>
+            <th onClick={handleSort}> Book</th>
+            <th>Authors</th>
             <th>Description</th>
             <th>Print Type</th>
           </tr>
         </thead>
-        <tbody style={{ justifyContent: 'center', textAlign: 'center' }}>
-          {books.map(book => (
-            <tr key={book.id}>
-              <td>{book.volumeInfo.title}</td>
-              <td>{book.volumeInfo.description}</td>
-              <td>{book.volumeInfo.printType}</td>
-            </tr>
-          ))}
+        <tbody style={{ justifyContent: "center", textAlign: "center" }}>
+          {books
+            .filter((book) => {
+              if (
+                !selectedAuthor.length ||
+                selectedAuthor == book.authors.join(", ")
+              ) {
+                return true;
+              }
+            })
+            .filter((book) => {
+              if (searchTerm === "") {
+                return book;
+              } else if (
+                book.title.toLowerCase().includes(searchTerm.toLowerCase())
+              ) {
+                return book;
+              }
+            })
+            .map((book) => {
+              return (
+                <tr style={{textAlign: "center", verticalAlign: "middle"}} key={book.id}>
+                  <td>{book.title}</td>
+                  <td>{book.authors.join(", ")}</td>
+                  <td>{book.description}</td>
+                  <td>{book.printType}</td>
+                </tr>
+              );
+            })}
         </tbody>
       </Table>
     </div>
@@ -91,5 +155,3 @@ const Epoch = () => {
 };
 
 export default Epoch;
-
-    
