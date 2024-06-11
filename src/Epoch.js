52b8d4eca9ae375163dc446c9ef6a59cbfcfb05d
OlyaBaeva <olyabaewa2014@mail.ru>
2024-06-11 12:43:44 +0300

    sort fixes

--- src/Epoch.js
+++ src/Epoch.js
@@ -74,84 +74,92 @@ const Epoch = () => {
   const handleClearFields = () => {
     setSearchTerm('');
   };
-
-  const handleSort = () => {
-    const booksCopy = [...books];
-    const sortedBooks = [...booksCopy].sort((a, b) => {
-      if (sortOrder === "asc") {
-        return a.title.localeCompare(b.title);
-      } else {
-        return b.title.localeCompare(a.title);
-      }
-    });
-
-    setBooks(sortedBooks);
-    setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+  
+  const handleSort = (e) => {
+    const sortType = e.target.value;
+    switch (sortType) {
+      case "default":
+        break;
+      case "alphabetical":
+        books.sort((a, b) => a.title.localeCompare(b.title));
+        break;
+      case "reverse-alphabetical":
+        books.sort((a, b) => b.title.localeCompare(a.title));
+        break;
+    }
+    setBooks([...books]);
   };
 
-  return (
-    <div>
-      <div className="container">
-        <figure className="text-end">
-          <blockquote className="blockquote">
-            <p> {quote} </p>
-          </blockquote>
-          <figcaption className="blockquote-footer">
-            <cite title="Source Title">{title}</cite>
-          </figcaption>
-        </figure>
+    return (
+      <div>
+        <div className="container">
+          <figure className="text-end">
+            <blockquote className="blockquote">
+              <p> {quote} </p>
+            </blockquote>
+            <figcaption className="blockquote-footer">
+              <cite title="Source Title">{title}</cite>
+            </figcaption>
+          </figure>
+        </div>
+        <select style={{ justifyContent: "center", textAlign: "center" }} className="form-select-sm" onClick={handleSelectPublisher}>
+          <option value="" >Author</option>
+          {uniqueAuthors.map((author) => (
+            <option key={books.id}>{author}</option>
+          ))}
+        </select>
+        <Button variant="outline-secondary float-end" size="sm" onClick={handleClearFields}>Clear Fields</Button>
+        <input className="form-control-sm float-end" type="text" value={searchTerm} onChange={handleChange} placeholder="Enter book title" style={{ marginLeft: "40px" }}/>
+        
+        <Table className="table" striped bordered hover variant="dark">
+          <thead style={{ textAlign: "center" }}>
+            <tr style={{textAlign: "center", verticalAlign: "middle"}}>
+              <th>
+                <select id="sort-select" style={{background: '#212529', color: '#fff', border: '#454d55',fontWeight: 'bold', justifyContent: "center", textAlign: "center" }} className="form-select-sm" onChange={handleSort}>
+          <option value="default">Default</option>
+          <option value="alphabetical">Alphabetical</option>
+          <option value="reverse-alphabetical">Reverse Alphabetical</option>
+          </select>
+        </th>
+              <th>Authors</th>
+              <th>Description</th>
+              <th>Print Type</th>
+            </tr>
+          </thead>
+          <tbody style={{ justifyContent: "center", textAlign: "center" }}>
+            {books
+              .filter((book) => {
+                if (
+                  !selectedAuthor.length ||
+                  selectedAuthor == book.authors.join(", ")
+                ) {
+                  return true;
+                }
+              })
+              .filter((book) => {
+                if (searchTerm === "") {
+                  return book;
+                } else if (
+                  book.title.toLowerCase().includes(searchTerm.toLowerCase())
+                ) {
+                  return book;
+                }
+              })
+              .map((book) => {
+                return (
+                  <tr style={{textAlign: "center", verticalAlign: "middle"}} key={book.id}>
+                    <td>{book.title}</td>
+                    <td>{book.authors.join(", ")}</td>
+                    <td>{book.description}</td>
+                    <td>{book.printType}</td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </Table>
+
       </div>
-      <select style={{ justifyContent: "center", textAlign: "center" }} className="form-select-sm" onClick={handleSelectPublisher}>
-        <option value="" >Author</option>
-        {uniqueAuthors.map((author) => (
-          <option key={books.id}>{author}</option>
-        ))}
-      </select>
-      <Button variant="outline-secondary float-end" size="sm" onClick={handleClearFields}>Clear Fields</Button>
-      <input className="form-control-sm float-end" type="text" value={searchTerm} onChange={handleChange} placeholder="Enter book title" style={{ marginLeft: "40px" }}/>
-      
-      <Table className="table" striped bordered hover variant="dark">
-        <thead style={{ textAlign: "center" }}>
-          <tr style={{textAlign: "center", verticalAlign: "middle"}}>
-            <th onClick={handleSort}> Book</th>
-            <th>Authors</th>
-            <th>Description</th>
-            <th>Print Type</th>
-          </tr>
-        </thead>
-        <tbody style={{ justifyContent: "center", textAlign: "center" }}>
-          {books
-            .filter((book) => {
-              if (
-                !selectedAuthor.length ||
-                selectedAuthor == book.authors.join(", ")
-              ) {
-                return true;
-              }
-            })
-            .filter((book) => {
-              if (searchTerm === "") {
-                return book;
-              } else if (
-                book.title.toLowerCase().includes(searchTerm.toLowerCase())
-              ) {
-                return book;
-              }
-            })
-            .map((book) => {
-              return (
-                <tr style={{textAlign: "center", verticalAlign: "middle"}} key={book.id}>
-                  <td>{book.title}</td>
-                  <td>{book.authors.join(", ")}</td>
-                  <td>{book.description}</td>
-                  <td>{book.printType}</td>
-                </tr>
-              );
-            })}
-        </tbody>
-      </Table>
-    </div>
-  );
-};
+    );
+  };
 
 export default Epoch;
